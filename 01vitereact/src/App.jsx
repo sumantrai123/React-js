3aa64c8c595c9afede37e7d3eac22bfe8d14ef7f
sumantrai123 <raisumant226@gmail.com>
2025-08-16 18:08:58 +0530

    custom react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,14 @@
 import Chai from "./chai";
 
+
 function App() {
+
+  const username = "Chai aur Code"
+
   return (
     <>
       <Chai />
-      <h1>Chai aur React</h1>
+      <h1>Chai aur React {username}</h1>
       <p>test para</p>
     </>
   );
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,42 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom React</h1>
+    </div>
+  )
+}
+
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://instagram.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to vist instagram'
+// }
+
+const anotherElement = (
+    <a href="https://instagram.com" target='_blank'>visit instagram</a>
+)
+
+const anotherUser = "chai aur react"
+
+const reactElement = React.createElement(
+    'a',
+    {href: "https://instagram.com", target: "_blank"},
+    'click to open instagram',
+    anotherUser
+)
+
 createRoot(document.getElementById('root')).render(
 
-    <App />
+    // ReactElement
+    // anotherElement
+    reactElement
+    // <App/>
 
 )
